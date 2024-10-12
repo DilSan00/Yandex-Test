45b767541a0221fc037685e28bf0393943d2fe8a
DilSan <norms777666@gmail.com>
2024-10-12 13:56:52 +0300

    fix bag mobile touch

--- js/desctop.js
+++ js/desctop.js
@@ -3,7 +3,7 @@ const basket = document.getElementById("basket");
 const basketSave = document.getElementById("basket-save");
 const button = document.querySelector(".btn");
 
-let itemCount = 0; 
+let itemCount = 0;
 
 products.forEach((product) => {
   product.addEventListener("dragstart", (event) => {
@@ -13,14 +13,24 @@ products.forEach((product) => {
 });
 
 basket.addEventListener("dragover", (event) => {
-  event.preventDefault(); 
+  event.preventDefault();
 });
 
 basket.addEventListener("drop", (event) => {
-  event.preventDefault(); 
+  event.preventDefault();
 
-  const offsetX = event.clientX - basket.getBoundingClientRect().left - 10;
-  const offsetY = event.clientY - basket.getBoundingClientRect().top - 10;
+  let offsetX = event.clientX - basket.getBoundingClientRect().left - 10;
+  let offsetY = event.clientY - basket.getBoundingClientRect().top - 10;
+
+  if (offsetY > 140) {
+    offsetY = 140;
+  }
+  if (offsetX > 220) {
+    offsetX = 220;
+  }
+  if (offsetX < 80) {
+    offsetX = 80;
+  }
 
   const newProduct = document.createElement("div");
   newProduct.classList.add("product");
@@ -29,20 +39,9 @@ basket.addEventListener("drop", (event) => {
   newProduct.style.left = `${offsetX}px`;
   newProduct.style.top = `${offsetY}px`;
   newProduct.draggable = true;
+  newProduct.id = `product-${itemCount}`;
 
   basketSave.appendChild(newProduct);
-
-  newProduct.addEventListener("dragstart", (event) => {
-    event.dataTransfer.setData("text/plain", event.target.outerHTML);
-  });
-
-  const originalProduct = [...products].find(
-    (product) =>
-      product.innerHTML === event.dataTransfer.getData("text/product-id")
-  );
-  if (originalProduct) {
-    originalProduct.remove();
-  }
 
   itemCount++;
   button.style.display = itemCount >= 3 ? "block" : "none";
